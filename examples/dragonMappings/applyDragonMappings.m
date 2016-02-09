0462290bdbd79dd17a17c9bb481469ffc372943f
Ben Heasly <benjamin.heasly@gmail.com>
2016-02-09 18:10:33 -0500

    sandbox getting names of mexximp scene elements and mappings

--- examples/dragonMappings/applyDragonMappings.m
+++ examples/dragonMappings/applyDragonMappings.m
@@ -13,10 +13,39 @@ clc;
 thisFolder = fileparts(which('applyDragonMappings.m'));
 sceneFile = fullfile(thisFolder, 'Dragon.blend');
 scene = mexximpImport(sceneFile);
-mexximpSceneScatter(scene);
+%mexximpSceneScatter(scene);
 
 % Flatten the node hierarchy, for sanity.
 flatScene = scene;
 flatScene.rootNode = mexximpFlattenNodes(flatScene);
-mexximpSceneScatter(flatScene);
+%mexximpSceneScatter(flatScene);
 
+%% Dig out names of materials.
+%   want util to put materials in standard, "flat" form?
+nMaterials = numel(flatScene.materials);
+materialNames = cell(1, nMaterials);
+isName = @(s) strcmp(s, 'name');
+for ii = 1:nMaterials
+    q = {'key', isName};
+    p = {'materials', ii, 'properties', q, 'data'};
+    materialNames{ii} = mPathGet(flatScene, p);
+end
+
+%% Get all the names of things in the scene.
+sceneNames = { ...
+    flatScene.cameras.name, ...
+    materialNames{:}, ...
+    flatScene.meshes.name, ...
+    flatScene.rootNode.name, ...
+    flatScene.rootNode.children.name};
+
+%% Get all the names of things subject to mappings.
+mappingsFile = fullfile(thisFolder, 'DragonMappings.txt');
+mappings = ParseMappings(mappingsFile);
+objects = MappingsToObjects(mappings);
+mappingsNames = {objects.id};
+
+%% For each name in the mappings, can we pick the correct scene name?
+%   use substring of?
+%   use edit distance?
+%   use mappings class/subclass as a hint?
