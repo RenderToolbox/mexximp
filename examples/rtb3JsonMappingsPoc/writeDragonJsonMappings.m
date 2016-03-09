005b3b4ab1438cb65ef90e1f265a3f60c699eae9
Ben Heasly <benjamin.heasly@gmail.com>
2016-03-09 18:19:53 -0500

    proof of concept sandbox for JSON mappings syntax

---
+++ examples/rtb3JsonMappingsPoc/writeDragonJsonMappings.m
@@ -0,0 +1,165 @@
+%% Sandbox.  Dragon scene mappings as Matlab struct -> JSON file.
+%
+% I am thinking about a new way to do rtb mappings, in the world of
+% Mexximp.
+%
+% I think we should no longer do our own text parsing.  Instead,
+% we should use JSON.  This way we can delete parsing code and use jsonlab
+% instead.  We can also convert JSON <-> struct in both directions.  We can
+% also access mappings with other tools that understand JSON, if that ever
+% comes up.
+%
+% So here is a stab at some struct/JSON mappings.  I'll to reproduce
+% our original DragonMappings.txt, but in this new, shinier style.
+%
+% Here are some additional differences I want to introduce:
+%
+% - Instead of treating mappings line by line, add some structure: some
+%   top-level structure to identify the scene element of interest and what
+%   to do with it (create it fresh, find it and update it, delete it,
+%   others?).  Then some nested structure to specify zero or more
+%   properties of that element.  We already gropu delcarations and
+%   properties when processing mappings now.  Let's just make this clear
+%   and explicit in the mappings syntax.
+%
+% - Instead of identifying elements by id matching, which is brittle,
+%   allow a few ways to identify elements: by name/id fuzzy matching, by
+%   type, and by index.  Specifying all three is overkill.  Sometimes it
+%   will make most sense to use an element's name, like when looking at a
+%   name specified in Blender.  Sometimes it will be good to provide the
+%   element's type as well, to narrow the search and eliminate false
+%   matches.  Sometimes it will be more natural to identify an element by
+%   tpye and index, like when looking at a mexximp scene struct.
+%   Sometimes, just the type will do, like when referring to the single
+%   camera and we don't care what its name is.  So name, type, and index
+%   will proscribe a well-defined element *search*, rather than a
+%   brittle id match.
+%
+
+
+%% For reference, I want to look at the original Dragon scene.
+clear;
+clc;
+
+originalScene = which('Dragon.blend');
+scene = mexximpImport(originalScene);
+scene.rootNode = mexximpFlattenNodes(scene);
+
+
+%% In the old Collada Mappings, we sometimes need to flip coordinates.
+% Collada {
+%     % swap camera handedness from Blender's Collada output
+%     Camera:scale|sid=scale = -1 1 1
+% }
+%
+% What we can do now is edit the root node instead of the camera.
+
+% top-level structure to identify the scene element and operation
+flip.name = '';
+flip.index = 1;
+flip.specificType = '';
+flip.broadType = 'rootNode';
+flip.operation = 'update';
+
+% zero or more nested properties of the element
+flip.properties(1).name = 'transformation';
+flip.properties(1).valueType = 'matrix';
+flip.properties(1).value = mexximpScale([-1 1 1]);
+
+
+%% We need to "bless" two existing meshes to make them area lights.
+% Generic {
+%     % make area lights with daylight spectrum
+%     LightX-mesh:light:area
+%     LightX-mesh:intensity.spectrum = D65.spd
+%     LightY-mesh:light:area
+%     LightY-mesh:intensity.spectrum = D65.spd
+%     ...
+% }
+
+blessX.name = 'LightX';
+blessX.index = 0;
+blessX.broadType = 'meshes';
+blessX.specificType = '';
+blessX.operation = 'blessThisMesh';
+blessX.properties(1).name = 'intensity';
+blessX.properties(1).valueType = 'spectrum';
+blessX.properties(1).value = 'D65.spd';
+
+blessY.name = 'LightY';
+blessY.index = 0;
+blessY.broadType = 'meshes';
+blessY.specificType = '';
+blessY.operation = 'blessThisMesh';
+blessY.properties(1).name = 'intensity';
+blessY.properties(1).valueType = 'spectrum';
+blessY.properties(1).value = 'D65.spd';
+
+
+%% We need set the type and spectrum for two materials.
+% Generic {
+%     ...
+%     % make the area lights perfect reflectors, too
+%     ReflectorMaterial-material:material:matte
+%     ReflectorMaterial-material:diffuseReflectance.spectrum = 300:1.0 800:1.0
+%
+%     % make gray walls and floor
+%     WallMaterial-material:material:matte
+%     WallMaterial-material:diffuseReflectance.spectrum = 300:0.75 800:0.75
+%     FloorMaterial-material:material:matte
+%     FloorMaterial-material:diffuseReflectance.spectrum = 300:0.5 800:0.5
+%
+%     % make a tan dragon
+%     DragonMaterial-material:material:matte
+%     DragonMaterial-material:diffuseReflectance.spectrum = mccBabel-1.spd
+%     ...
+% }
+
+materials(1).name = 'ReflectorMaterial';
+materials(1).index = 0;
+materials(1).broadType = 'materials';
+materials(1).specificType = 'matte';
+materials(1).operation = 'update';
+materials(1).properties(1).name = 'diffuseReflectance';
+materials(1).properties(1).valueType = 'spectrum';
+materials(1).properties(1).value = '300:1.0 800:1.0';
+
+materials(2).name = 'WallMaterial';
+materials(2).index = 0;
+materials(2).broadType = 'materials';
+materials(2).specificType = 'matte';
+materials(2).operation = 'update';
+materials(2).properties(1).name = 'diffuseReflectance';
+materials(2).properties(1).valueType = 'spectrum';
+materials(2).properties(1).value = '300:0.75 800:0.75';
+
+materials(3).name = 'FloorMaterial';
+materials(3).index = 0;
+materials(3).broadType = 'materials';
+materials(3).specificType = 'matte';
+materials(3).operation = 'update';
+materials(3).properties(1).name = 'diffuseReflectance';
+materials(3).properties(1).valueType = 'spectrum';
+materials(3).properties(1).value = '300:0.5 800:0.5';
+
+materials(4).name = 'DragonMaterial';
+materials(4).index = 0;
+materials(4).broadType = 'materials';
+materials(4).specificType = 'matte';
+materials(4).operation = 'update';
+materials(4).properties(1).name = 'diffuseReflectance';
+materials(4).properties(1).valueType = 'spectrum';
+materials(4).properties(1).value = 'mccBabel-1.spd';
+
+
+%% Now we can write the mappings file.
+% Just pack up all the mappings as a struct array and dump out to JSON.
+% That's it!
+allMappings.Generic = cat(2, flip, blessX, blessY, materials);
+
+pathHere = fileparts(which('writeDragonJsonMappings'));
+mappingsFile = fullfile(pathHere, 'DragonJsonMappings.json');
+savejson('', allMappings, ...
+    'FileName', mappingsFile, ...
+    'ArrayIndent', 1, ...
+    'ArrayToStrut', 0);
