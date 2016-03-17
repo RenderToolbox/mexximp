6113e0ca45b1d0e0390328e7f2f37ef1ab772694
Ben Heasly <benjamin.heasly@gmail.com>
2016-03-17 16:58:12 -0400

    convert mexximp nodes to mPbrt, test rendering!

---
+++ examples/rtb3JsonMappingsPoc/mexximpToPbrt/mexximpNodeToMPbrt.m
@@ -0,0 +1,50 @@
+function [pbrtNode, nObjects] = mexximpNodeToMPbrt(scene, node, varargin)
+%% Convert a mexximp node to an mPbrt ObjectInstance and transformation.
+%
+% pbrtNode = mexximpNodeToMPbrt(scene, node, varargin) converts the given
+% mexximp node to an mPbrt Attribute container which includes the
+% node's transformation and one ObjectInstance for each of the node's
+% meshIndices.
+%
+% Returns an Attribute MPbrtContainer that includes the node's
+% transformation and zero or more ObjectInstance elements.  Also returns
+% the number of object instance elements.
+%
+% [pbrtNode, nObjects] = mexximpNodeToMPbrt(scene, node, varargin)
+%
+% Copyright (c) 2016 mexximp Team
+
+parser = rdtInputParser();
+parser.addRequired('scene', @isstruct);
+parser.addRequired('node', @isstruct);
+parser.parse(scene, node, varargin{:});
+scene = parser.Results.scene;
+node = parser.Results.node;
+
+%% Dig out the name.
+nodeName = node.name;
+nodeIndex = node.path{end};
+pbrtName = mexximpCleanName(nodeName, nodeIndex);
+
+%% Add the node transformation to an Attribute.
+data = mPathGet(scene, node.path);
+
+pbrtNode = MPbrtContainer('Attribute', 'comment', pbrtName);
+pbrtTransformation = MPbrtElement.transformation('ConcatTransform', data.transformation');
+pbrtNode.append(pbrtTransformation);
+
+%% Follow 0-based indices to instantiated meshes.
+nObjects = numel(data.meshIndices);
+
+if 0 == nObjects
+    % no objects to instantiate!
+    return;
+end
+
+meshIndices = data.meshIndices + 1;
+for oo = 1:nObjects
+    meshIndex = meshIndices(oo);
+    meshName = mexximpCleanName(scene.meshes(meshIndex).name, meshIndex);
+    pbrtObjectInstance = MPbrtElement('ObjectInstance', 'value', meshName);
+    pbrtNode.append(pbrtObjectInstance);
+end
